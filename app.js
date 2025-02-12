88b22fe76183631b30a8b2bb0478dbe18b724099
Angel Mauricio <168794154+El-Rincon-Del-Cine@users.noreply.github.com>
2025-02-11 18:27:12 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/ws.js')
+        navigator.serviceWorker.register('./ws.js')
             .then((registration) => {
                 console.log('Service Worker registrado', registration.scope);
             })
