da313090f48e92e1074d5c1fd79658fa7ae49931
Angel Mauricio <168794154+El-Rincon-Del-Cine@users.noreply.github.com>
2025-02-11 18:42:21 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('./ws.js')
-            .then((registration) => {
-                console.log('Service Worker registrado', registration.scope);
-            })
-            .catch((error) => {
-                console.log('Error al registrar el Service Worker:', error);
-            });
-    });
-}
+    navigator.serviceWorker.register('./ws.js', {scope: './'})
+        .then(function(registration){
+            console.log('Services Worker registrdo!!', registration);
+        })
+        .catch(function(error) {
+            console.log('Error al registrar el service worker', error);
+        });
+    }
